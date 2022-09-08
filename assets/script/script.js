ff3a6da1a9fbf11b5536c44a6e62305897e5d078
Lucas Renan <lucasrenandns96@gmail.com>
2022-09-08 11:58:31 -0300

    Scroll
    Scroll suave

---
+++ assets/script/script.js
@@ -0,0 +1,22 @@
+
+const button = document.getElementById("button")
+
+button.addEventListener("click", function() {
+    window.scrollTo(0, 0)
+})
+
+document.addEventListener("scroll", Hidden)
+
+function Hidden() {
+    
+    if(window.scrollY > 1500) {
+        button.style.display = "flex"
+    }
+    else {
+        button.style.display = "none"
+    }
+   
+}
+
+Hidden()
+
